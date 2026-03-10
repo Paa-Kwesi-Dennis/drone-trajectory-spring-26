dd42c5ce949c06be20d8f49c56352f91742bf1c5
Paa Kwesi Dennis <dennispaa9127@gmail.com>
2026-03-10 18:45:39 -0400

    Data model for DatasetSpec

--- src/data_model.py
+++ src/data_model.py
@@ -4,6 +4,13 @@ class DatasetSpec:
     """
     Data model for specifications of an image dataset.
     """
+    def __init__(self, overlap, sidelap, height, scan_dimension_x, scan_dimension_y, exposure_time_m):
+        self.overlap = overlap
+        self.sidelap = sidelap
+        self.height = height 
+        self.scan_dimension_x = scan_dimension_x
+        self.scan_dimension_y = scan_dimension_y
+        self.exposure_time_m = exposure_time_m
     pass
 
 
@@ -23,3 +30,16 @@ class Waypoint:
     Waypoints are positions where the drone should fly to and capture a photo.
     """
     pass
+
+# Model the nomimal dataset spec
+
+overlap = 0.7
+sidelap = 0.7
+height = 30.48 # 100 ft
+scan_dimension_x = 150
+scan_dimension_y = 150
+exposure_time_ms = 2 # 1/500 exposure time
+
+dataset_spec = DatasetSpec(overlap, sidelap, height, scan_dimension_x, scan_dimension_y, exposure_time_ms)
+
+print(f"Nominal specs: {dataset_spec}")
